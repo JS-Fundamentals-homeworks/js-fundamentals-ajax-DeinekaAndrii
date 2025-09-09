72d7a1f69cb03e739135461f535318df6193ced4
DeinekaAndrii <andrew.deineka@gmail.com>
2025-09-09 21:33:53 +0300

    task1

--- task1.js
+++ task1.js
@@ -5,3 +5,22 @@
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
 // Перевірити за допомогою команди npm tests/task1.test.js 
+
+const usersList = document.querySelector('.usersList');
+
+async function getUser(){
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        if(!response.ok){ throw new Error(`HTTP error! status: ${response.status}`); }
+        const users = await response.json();
+        users.forEach(element => {
+            const li = document.createElement('li');
+            li.textContent = element.name;
+            usersList.appendChild(li);
+        });
+    }catch(error){
+        console.error('Fetch error: ', error);
+        return;
+    }
+}
+getUser();
